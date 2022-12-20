a1ce59e035abd2b69942f02a3bee27e91b2cc8e9
wadzamurombedzi <wadzanaimurombedzi0@gmail.com>
2022-12-20 04:53:45 -0800

    Wadzanai Murombedzi and Mkize Zamambo simple shell

--- shell_loop.c
+++ shell_loop.c
@@ -65,7 +65,7 @@ int find_builtin(info_t *info)
 		{"cd", _mycd},
 		{"alias", _myalias},
 		{NULL, NULL}
-	};
+	}
 
 	for (i = 0; builtintbl[i].type; i++)
 		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
